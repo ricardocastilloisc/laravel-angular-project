983b9861c6e11c54c9a43f2937b687c735efbafc
ricardocastilloisc <ricardocastilloisc@gmail.com>
2016-08-05 11:51:35 -0500

    Aun con configuraciones
    Documentación con
    
    https://github.com/tymondesigns/jwt-auth/wiki/Installation
    
    https://github.com/barryvdh/laravel-cors

--- api/app/Http/Controllers/ApiAuthController.php
+++ api/app/Http/Controllers/ApiAuthController.php
@@ -8,5 +8,19 @@ use App\Http\Requests;
 
 class ApiAuthController extends Controller
 {
+	public function UserAuth(Request $request)
+	{
+		$credentials = $request->only('email', 'password');
+		$token = null;
+		try {
+			if(!$token = JWTAuth::attempt($credentials))
+			{
+				return response()->json(['error' => 'invalid_credentials'])
+			}
+			
+		} catch (JWTException $ex) {
+			return response()->json(['error' => 'something_went_wrog'], 500);
+		}
+	}
     //
 }
